b9a4ee200bc6721e22356c91276d1461c0faf043
sylvain121 <sylvain121@gmail.com>
2017-10-04 06:25:41 +0000

    performance test

--- encoder_ffmpeg.c
+++ encoder_ffmpeg.c
@@ -91,7 +91,7 @@ extern "C" {
 		 * will always be I frame irrespective to gop_size
 		 */
 		c->gop_size = 100;
-		c->max_b_frames = 1;
+		//c->max_b_frames = 1;
 		c->refs = 0;
 		//c->pix_fmt = AV_PIX_FMT_YUV420P; //AV_PIX_FMT_YUV444P;
 
@@ -108,7 +108,8 @@ extern "C" {
 		av_opt_set(c->priv_data, "preset", "ultrafast", 0);
 		av_opt_set(c->priv_data, "tune", "zerolatency", 0);
 		av_opt_set(c->priv_data, "profile", "baseline", 0);
-		//av_opt_set(c->priv_data, "movflags", "faststart", 0);
+		c->level = 32;
+			//av_opt_set(c->priv_data, "movflags", "faststart", 0);
 		av_opt_set(c->priv_data, "x264opts", "no-mbtree:sliced-threads:sync-lookahead=0", 0);
 		/* open it */
 		if (avcodec_open2(c, codec, NULL) < 0) {
