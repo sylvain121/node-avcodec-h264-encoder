594203cb8864420bf615f01f911869e38194f64d
esme7383 <sylvain.mathieu@orange.com>
2020-05-19 17:56:56 +0200

    update typescript definition

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,20 @@
 declare module 'node-avcodec-h264-encoder' {
-    function init(inputWidth: number, inputHeight: number, outputWidth: number, outputHeight: number, bitrate: number, fps: number, pix_fmt_int: number);
-    function encodeFrame(sourceFrame: Buffer): Buffer;
+
+    interface AvcodecH264Encoder {
+        inputWidth: number;
+        inputHeight: number;
+        outputWidth: number;
+        outputHeight: number;
+        bit_rate: number;
+        fps: number;
+        sample: number;
+    }
+
+
+    const YUV_420P = 1;
+    const YUV_444P = 2;
+
+    function initSync(inputWidth: number, inputHeight: number, outputWidth: number, outputHeight: number, bitrate: number, fps: number, pix_fmt_int: number);
+    function encodeFrameSync(sourceFrame: Buffer): Buffer;
+
 }
